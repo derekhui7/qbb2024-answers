7453508202b7a6e07353d602287e364a492b5b91
Derek Hui <mhui3@jh.edu>
2024-10-03 12:45:10 -0400

    Updated exercise 3 R file

--- week3-all/ex3.R
+++ week3-all/ex3.R
@@ -8,11 +8,11 @@ DP <- read_tsv("/Users/cmdb/qbb2024-answers/week3-all/DP.txt")
 AF_plot <- ggplot(AF, aes(x = Allele_Frequency)) + 
   geom_histogram(bins = 11) + 
   labs(x = "Allele Frequency", y = "Number of Variation", title = "Allele Frequency")
-ggsave("/Users/cmdb/qbb2024-answers/week2-all/AF_plot.png", plot = AF_plot)
+ggsave("/Users/cmdb/qbb2024-answers/week3-all/AF_plot.png", plot = AF_plot)
 
 #Create a histogram with the ggplot
 DP_plot <- ggplot(DP, mapping = aes(x = Read_Depth)) + 
   geom_histogram(bins = 21) + 
   xlim(0, 20) + 
   labs(x = "Read Depth", y = "Number of Variation", title = "Read Depth")
-ggsave("/Users/cmdb/qbb2024-answers/week2-all/DP_plot.png", plot = DP_plot)
+ggsave("/Users/cmdb/qbb2024-answers/week3-all/DP_plot.png", plot = DP_plot)
